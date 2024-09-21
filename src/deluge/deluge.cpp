8ffda36cdf119c1be0f54654b5eff0a9d029d82e
bobtwinkles <srkoser+github@gmail.com>
2024-09-21 11:05:44 -0700

    wip: actually build tinyusb sources
    this is horribly incomplete and feels super hacky but at least it tries
    to build the right parts of tinyusb

--- src/deluge/deluge.cpp
+++ src/deluge/deluge.cpp
@@ -74,6 +74,7 @@
 #include <stdlib.h>
 
 #include "RZA1/cache/cache.h"
+#include "tusb.h"
 
 extern "C" {
 #include "RZA1/gpio/gpio.h"
@@ -111,6 +112,11 @@ int32_t voltageReadingLastTime = 65535 * 3300;
 uint8_t batteryCurrentRegion = 2;
 uint16_t batteryMV;
 bool batteryLEDState = false;
+
+// TODO: Move this to somewhere better
+void usb_int(uint32_t sense) {
+	tud_int_handler(0);
+}
 
 void batteryLEDBlink() {
 	setOutputState(BATTERY_LED.port, BATTERY_LED.pin, batteryLEDState);
@@ -793,6 +799,9 @@ extern "C" int32_t deluge_main(void) {
 	    == RuntimeFeatureStateEmulatedDisplay::OnBoot) {
 		deluge::hid::display::swapDisplayType();
 	}
+
+	R_INTC_RegistIntFunc(INTC_ID_USBI0, usb_int);
+	tusb_init();
 
 	{
 		deluge::io::usb::USBAutoLock lock;
